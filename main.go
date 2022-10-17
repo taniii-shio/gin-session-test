f6a057d7e06a1257ec8370716736a28e6f2d9579
shioya naoto <shioya.naoto17@gmail.com>
2022-10-17 15:33:40 +0900

    edit logic

--- main.go
+++ main.go
@@ -1,13 +1,31 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
+
 
 func main() {
     r := gin.Default()
-    r.GET("/ping", func(c *gin.Context) {
-        c.JSON(200, gin.H{
-            "message": "pong",
-        })
-    })
+
+		r.GET("/login", func(c *gin.Context) {
+			sessionID := uuid.New().String()
+
+			c.SetCookie("sessionID", sessionID, 3600, "/", "localhost", false, true)
+			c.JSON(200, gin.H{ "message": "cookieをセットしました。"  })
+		})
+
+		r.GET("/callback", func(c *gin.Context) {
+			cookie, err := c.Cookie("sessionID") 
+
+			if err != nil {
+				c.JSON(200, gin.H{ "message": "sessionIDを取得できません。" })
+				return
+			}
+
+			c.JSON(200, gin.H{ "message": "sessionIDを取得しました。" + cookie })
+		})
+		
     r.Run()
 }
